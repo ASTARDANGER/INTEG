ed9b132db958797d3cd1d4f9b63b9ac0905a7e09
ASTARDANGER <aastardanger@gmail.com>
2025-02-12 16:46:30 +0100

    pour robinson mais c vraiment pck c lui

--- calibration.py
+++ calibration.py
@@ -6,11 +6,26 @@ import glob
 import json
 #import pathlib
 
+def sort_corners(corners, nx, ny):
+    # Trier les coins en fonction de leur position sur l'image
+    sorted_corners = []
+    for i in range(ny):
+        # Trier par ligne : trier les coins de chaque ligne en fonction de leur coordonnée x
+        row_corners = sorted(corners[i*nx:(i+1)*nx], key=lambda x: x[0][0])
+        sorted_corners.extend(row_corners)
+    
+    # Convertir en array NumPy de type float32, qui est le format attendu
+    return np.array(sorted_corners, dtype=np.float32)
+
+
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+# prepare object points, like (0,0,0), (0.02,0,0), (0.04,0,0) ....,(0.12,0.12,0)
+square_size = 0.02  # in mm
+nx, ny = 7, 7 # number of corners in x and y directions
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = square_size*np.mgrid[0:nx,0:ny].T.reshape(-1,2)
+print(objp)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -31,9 +46,10 @@ for fname in images:  # Here, 10 can be changed to whatever number you like to c
     if ret == True:
         objpoints.append(objp)   # Certainly, every loop objp is the same, in 3D.
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+        sorted_corners = sort_corners(corners2, nx, ny)
+        imgpoints.append(sorted_corners)
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,7), corners2, ret)
+        img = cv2.drawChessboardCorners(img, (7,7), sorted_corners, ret)
         found += 1
         cv2.imshow('img', img)
         cv2.waitKey(500)
@@ -53,7 +69,9 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 
 # transform the matrix and distortion coefficients to writable lists
 data = {'camera_matrix': np.asarray(mtx).tolist(),
-        'dist_coeff': np.asarray(dist).tolist()}
+        'dist_coeff': np.asarray(dist).tolist(),
+        'rvecs': np.asarray(rvecs).tolist(),
+        'tvecs': np.asarray(tvecs).tolist()}
 
 # and save it to a file
 with open("calibration_matrix.json", "w") as f:
